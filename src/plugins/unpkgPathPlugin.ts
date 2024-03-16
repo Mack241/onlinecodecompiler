e2ccd0d29ea9e149bfd5ed25d348e6cc170d69d6
Mayukh <mayukhdasgupta19@gmail.com>
2024-03-16 19:08:25 +0530

    Create multiple 'build.onResolve()' for better code handling

--- src/plugins/unpkgPathPlugin.ts
+++ src/plugins/unpkgPathPlugin.ts
@@ -10,19 +10,21 @@ export const unpkgPathPlugin = (input: string) => {
    return {
       name: "unpkg-path-plugin",
       setup(build: esbuild.PluginBuild) {
+         // handle root entry file of index.js
+         build.onResolve({ filter: /(^index\.js$)/ }, () => {
+            return { path: 'index.js', namespace: 'a' }
+         })
+
+         // handle relative paths in a module
+         build.onResolve({ filter: /^\.+\// }, (args: any) => {
+            return {
+               namespace: 'a',
+               path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
+            }
+         })
+
+         // handle main file of a module
          build.onResolve({ filter: /.*/ }, async (args: any) => {
-            console.log('on Resolve', args)
-            if (args.path == "index.js") {
-               return { path: args.path, namespace: 'a' };
-            }
-
-            if (args.path.includes('./') || args.path.includes('../')) {
-               return {
-                  namespace: 'a',
-                  path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/').href
-               }
-            }
-
             return {
                path: `https://unpkg.com/${args.path}`,
                namespace: 'a'
