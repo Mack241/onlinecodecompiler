1f8f51a8d9dfb6c81a661aa7ff330406e3c0a716
Mayukh <mayukhdasgupta19@gmail.com>
2024-03-20 22:49:05 +0530

    Add iframe to execute code

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ const App = () => {
     const [input, setInput] = useState('');
     const [code, setCode] = useState('');
     const ref = useRef<any>();
+    const iframe = useRef<any>();
 
     const startService = async () => {
         ref.current = await esbuild.startService({
@@ -43,8 +44,23 @@ const App = () => {
             }
         })
 
-        setCode(result.outputFiles[0].text)
+        // console.log(iframe.current.contentWindow)
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
     }
+
+    const html = `
+      <html>
+        <head></head>
+        <body>
+            <div id="root"></div>
+            <script>
+                window.addEventListener('message', (event) => {
+                    eval(event.data);
+                }, false)
+            </script>
+        </body>
+      </html>  
+    `;
 
     return (
         <div>
@@ -53,8 +69,10 @@ const App = () => {
                 <button onClick={onClick}>Submit</button>
             </div>
             <pre>{code}</pre>
+            <iframe ref={iframe} sandbox='allow-scripts' srcDoc={html} />
         </div>
     )
+
 }
 
 root.render(<App />)
